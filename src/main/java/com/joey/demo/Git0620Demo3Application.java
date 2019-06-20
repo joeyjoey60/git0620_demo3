b6977afd6515dfb2c48f8225dee4425f4547bf91
joey <2401318067@qq.com>
2019-06-20 15:12:31 +0800

    Initial commit

---
+++ src/main/java/com/joey/demo/Git0620Demo3Application.java
@@ -0,0 +1,13 @@
+package com.joey.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git0620Demo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git0620Demo3Application.class, args);
+	}
+
+}
